0ec6c5eb6a37bfe043150aa42ed924d8f7fe56cd
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-04-05 18:07:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-import streamlit as st 
+import streamlit as st
 from streamlit_chat import message
 import tempfile
-
 from langchain_community.document_loaders.csv_loader import CSVLoader
-
-
-from langchain.chains import ConversationalRetrievalChain
+from langchain_community.chains import ConversationalRetrievalChain
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import CTransformers
-
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
 # Carregando o modelo
 def carregar_llm():
-    # Carregar o modelo baixado localmente aqui
     llm = CTransformers(
         model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
         model_type="llama",
@@ -30,18 +25,13 @@ st.markdown("<h3 style='text-align: center; color: white;'>Construído por <a hr
 arquivo_enviado = st.sidebar.file_uploader("Envie seus Dados", type="csv")
 
 if arquivo_enviado:
-    # usar tempfile porque CSVLoader aceita apenas um file_path
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(arquivo_enviado.getvalue())
         tmp_file_path = tmp_file.name
 
-    carregador = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={
-                'delimiter': ','})
+    carregador = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','})
     dados = carregador.load()
-    # st.json(dados)
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs={'device': 'cpu'})
-
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
     db = FAISS.from_documents(dados, embeddings)
     db.save_local(DB_FAISS_PATH)
     llm = carregar_llm()
@@ -61,20 +51,16 @@ if arquivo_enviado:
     if 'past' not in st.session_state:
         st.session_state['past'] = ["Ei! 👋"]
         
-    # container para o histórico do chat
     response_container = st.container()
-    # container para a entrada de texto do usuário
     container = st.container()
 
     with container:
         with st.form(key='my_form', clear_on_submit=True):
-            
             entrada_usuario = st.text_input("Consulta:", placeholder="Fale com seus dados csv aqui (:", key='input')
             botao_enviar = st.form_submit_button(label='Enviar')
             
         if botao_enviar and entrada_usuario:
             saida = chat_conversacional(entrada_usuario)
-            
             st.session_state['past'].append(entrada_usuario)
             st.session_state['generated'].append(saida)
 
