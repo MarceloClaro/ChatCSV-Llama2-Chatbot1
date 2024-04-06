d10ef778939fed4809d0c8af9b01da5d5b5e00e4
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-04-06 02:52:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests               # Para fazer requisições HTTP, como baixar arqui
 import os                     # Para interagir com o sistema de arquivos do sistema operacional
 
 # Inserir o logotipo acima do título "Navegação"
-st.image("logo.svg", use_column_width=True)
+st.image("LOGOS_INARTE_COM_SLOGAN.svg", use_column_width=True)
 # Caminho onde será armazenado o banco de dados do vetor FAISS
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
