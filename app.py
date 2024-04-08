3f6ee1b69b9502388f207d1969f5e69d0c814277
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-04-07 21:40:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_chat import message
 import tempfile
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceEmbeddings
@@ -9,24 +8,12 @@ from langchain.chains import ConversationalRetrievalChain
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-def load_llm():
-    llm = CTransformers(
-        model="llama-2-7b-chat.ggmlv3.q8_0.bin",
-        model_type="llama",
-        max_new_tokens=512,
-        temperature=0.5
-    )
-    return llm
-
 def process_uploaded_file(uploaded_file):
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(uploaded_file.read())
         tmp_file_path = tmp_file.name
 
-    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','})
-    data = loader.load()
-
-    return data
+    return tmp_file_path
 
 def setup_chat():
     st.title("🦙 Chat com CSV usando Llama2 🦜")
@@ -39,26 +26,26 @@ def setup_chat():
     st.sidebar.write("Faça upload do arquivo CSV:")
     uploaded_file = st.sidebar.file_uploader("Carregar seus Dados", type="csv")
 
-    if uploaded_file:
-        data = process_uploaded_file(uploaded_file)
-        return data, username, password, repo_id
-    else:
-        return None, None, None, None
+    st.sidebar.write("Faça upload do modelo Nous-Hermes:")
+    model_file = st.sidebar.file_uploader("Carregar modelo Nous-Hermes", type="gguf")
 
-def conversational_chat(user_input, chain):
-    output = chain.predict(user_input)
-    return output
+    if uploaded_file and model_file:
+        data_path = process_uploaded_file(uploaded_file)
+        model_path = process_uploaded_file(model_file)
+        return data_path, model_path, username, password, repo_id
+    else:
+        return None, None, None, None, None
 
 def main():
-    data, username, password, repo_id = setup_chat()
+    data_path, model_path, username, password, repo_id = setup_chat()
 
-    if data and username and password and repo_id:
+    if data_path and model_path and username and password and repo_id:
         embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
 
-        db = FAISS.from_documents(data, embeddings)
+        db = FAISS.from_documents(data_path, embeddings)
         db.save_local(DB_FAISS_PATH)
 
-        llm = load_llm()
+        llm = CTransformers(model=model_path, model_type="llama", max_new_tokens=512, temperature=0.5)
         chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
 
         if 'history' not in st.session_state:
@@ -79,15 +66,15 @@ def main():
                 submit_button = st.form_submit_button(label='Enviar')
 
             if submit_button and user_input:
-                output = conversational_chat(user_input, chain)
+                output = chain.predict(user_input)
                 st.session_state['past'].append(user_input)
                 st.session_state['generated'].append(output)
 
         if st.session_state['generated']:
             with response_container:
                 for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                    message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+                    st.write(st.session_state["past"][i])
+                    st.write(st.session_state["generated"][i])
 
 if __name__ == "__main__":
     main()
